a94deb472e26ce9d0888e6abeae34630f3b3ba5b
Sam Kahr <samkahr@gmail.com>
2019-07-10 18:01:02 -0700

    updating linke to CNN portfolio project

--- src/components/ItemFilter.vue
+++ src/components/ItemFilter.vue
@@ -40,8 +40,8 @@ export default {
 			{title: "Bloc Jams", overview: "Built a custom music player in React.", category: 'Front End', link: 'https://github.com/samkahr/bloc-jams-react' },
 			{title: "Bloc Chatroom", overview: "Using Firebase and React, created a chat room app", category: 'Front End', link: 'https://github.com/samkahr/bloc-chatroom'},
 			{title: "Digital Braille", overview: "Built a machine learning app that translates text in a picture to on screen brail. Built using Javascript and Google Clouds ML Engine", category: 'Machine Learning', link: 'https://github.com/dxh9845/braille-vibrate'},
-			{title: "Hot Dog or No Hot Dog", overview: "End to end machine learning app that takes a raw jpeg file and will determine whether image contains a hot dog or not. Built using TensorFlow and OpenCV", category: 'Machine Learning', link: ''},
-			{title: 'Video Object Recognition', overview: "Object detection using trained TensorFlow model, & exporting .mp4 with labels using OpenCV" , category: 'Machine Learning', link: 'https://github.com/samkahr/videoObjectRecognition/blob/master/Video%20Object%20Recognition.ipynb'},
+			{title: "Hot Dog or No Hot Dog", overview: "End to end machine learning app that takes a raw jpg file and will determine whether image contains a hot dog or not. Built using TensorFlow and OpenCV", category: 'Machine Learning', link: 'https://github.com/samkahr/hotdog_cnn'},
+			{title: 'Video Object Recognition', overview: "Object detection using trained TensorFlow model, & exporting .mp4 file with labels of items detected in video using OpenCV" , category: 'Machine Learning', link: 'https://github.com/samkahr/videoObjectRecognition/blob/master/Video%20Object%20Recognition.ipynb'},
       {title: 'Ad CTR Prediction', overview: "Created an end to end machine learning app that allows marketers to upload a CVS with ad targeting criteria and get click through rate predictions" , category: 'Machine Learning', link: 'https://github.com/kanalyzers/ad_optimization', link2:'https://kanalyzers.appspot.com/'},
 
 		]
